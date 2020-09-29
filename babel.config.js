8fb19c581652959ba4e2edfa25656a1762e48542
Nash Bansal <29092604+Trashpants@users.noreply.github.com>
2020-09-29 15:08:22 +0100

    Added some basic packages and setup .env and linter

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo", "@babel/preset-typescript"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".es", ".es6", ".mjs", ".ts", ".tsx"],
+          alias: {
+            "@assets": "./assets",
+            "@screens": "./src/views/screens",
+            "@navigators": "./src/views/navigators",
+            "@state": "./src/state",
+            "@utilities": "./src/utilities",
+            "@components": "./src/components",
+          },
+        },
+      ],
+      ["inline-dotenv"],
+    ],
   };
 };
